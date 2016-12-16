57cd15e81c9dc6da3e715547382238e6affa65f7
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-12-15 22:27:52 -0300

    Atividades 5 e 6 - Lendo dados do terminal usando TextReader e StreamReader

---
+++ Atividade5ConsoleAplication/Atividade5ConsoleAplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade5ConsoleAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá C# ");
+            while(true)
+            {
+                Console.WriteLine("Escreva alguma coisa ");
+                using (Stream entrada = Console.OpenStandardInput())
+                using (TextReader leitor = new StreamReader(entrada))
+                {
+                    var linha = leitor.ReadLine();
+                    if (linha != null)
+                    {
+                        Console.WriteLine("Você escreveu:");
+                        Console.WriteLine(linha);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
